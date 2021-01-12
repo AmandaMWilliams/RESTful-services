9e5bbad0aeb497ea53dd84f5e7c966f5bf30e082
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-12 18:06:36 -0500

    correct CrossOrigin URI

--- src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/HelloWorldController.java
+++ src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/HelloWorldController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-//CONTROLLER
-@CrossOrigin(origins = "http:/http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
