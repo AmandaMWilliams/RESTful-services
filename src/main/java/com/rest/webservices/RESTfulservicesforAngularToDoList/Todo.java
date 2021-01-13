df7ffd88e90fb4d963d3d7f875b26c92e7f644fa
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-13 17:17:50 -0500

    added REST API to delete a Todo

--- src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/Todo.java
+++ src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/Todo.java
@@ -1,6 +1,7 @@
 package com.rest.webservices.RESTfulservicesforAngularToDoList;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Todo {
     private Long id;
@@ -58,4 +59,21 @@ public class Todo {
 
     private Boolean isDone;
 
+    @Override
+    public boolean equals(Object obj) {
+        if(this==obj) return true;
+        if(obj == null) return false;
+        if(getClass() != obj.getClass()) return false;
+        Todo other = (Todo) obj;
+        if(id != other.id) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
 }
--- src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/TodoHardcodedService.java
+++ src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/TodoHardcodedService.java
@@ -13,13 +13,31 @@ public class TodoHardcodedService {
     private static List<Todo> todos = new ArrayList();
     private static int idCounter = 0;
 
-    static{
+    static {
         todos.add(new Todo((long) ++idCounter, "amandaw", "Learn REST", new Date(), false));
         todos.add(new Todo((long) ++idCounter, "amandaw", "Learn Angular", new Date(), false));
         todos.add(new Todo((long) ++idCounter, "amandaw", "Get a job", new Date(), false));
     }
 
-    public List<Todo> findAll(){
+    public List<Todo> findAll() {
         return todos;
+    }
+
+    public Todo deleteById(long id) {
+        Todo todo = findById(id);
+        if (todo == null) return null;
+        if (todos.remove(todo)) {
+            return todo;
+        }
+        return null;
+    }
+
+    public Todo findById(long id) {
+        for (Todo todo : todos) {
+            if (todo.getId() == id) {
+                return todo;
+            }
+        }
+        return null;
     }
 }
