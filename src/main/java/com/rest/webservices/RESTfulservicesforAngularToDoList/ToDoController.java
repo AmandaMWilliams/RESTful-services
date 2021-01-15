c50248e936b2b2f135167e20f507c73f21c2987d
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-15 17:46:15 -0500

    reformatted ToDoController

--- src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/ToDoController.java
+++ src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/ToDoController.java
@@ -21,6 +21,11 @@ public class ToDoController {
         return todoService.findAll();
     }
 
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo getTodo(@PathVariable String username, @PathVariable long id) {
+        return todoService.findById(id);
+    }
+
     @DeleteMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
         Todo todo = todoService.deleteById(id);
@@ -28,11 +33,6 @@ public class ToDoController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
-    }
-
-    @GetMapping("/users/{username}/todos/{id}")
-    public Todo getTodo(@PathVariable String username, @PathVariable long id) {
-        return todoService.findById(id);
     }
 
     @PutMapping("/users/{username}/todos/{id}")
