892546e71fe54b45c5f338bbe0253642343cea21
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-20 16:51:55 -0500

    added BasicAuthController and AuthenticationBean classes

---
+++ src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/basic/auth/AuthenticationBean.java
@@ -0,0 +1,25 @@
+package com.rest.webservices.RESTfulservicesforAngularToDoList.basic.auth;
+
+public class AuthenticationBean {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public AuthenticationBean(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
