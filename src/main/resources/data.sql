3caa31f0c6d403145ff64f074a5caca583c42da1
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-25 18:23:24 -0500

    populated database in data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values(10001, 'amandaw', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10002, 'amandaw', 'Learn Date JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10003, 'amandaw', 'Learn Microservices', sysdate(), false);
