25019667efe0a71fb0d5bd380f9200b991d0bdbf
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-25 16:03:36 -0500

    updated bcrypt password in JwtInMemoryUserDetailsService class

--- src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/jwt/JwtInMemoryUserDetailsService.java
+++ src/main/java/com/rest/webservices/RESTfulservicesforAngularToDoList/jwt/JwtInMemoryUserDetailsService.java
@@ -16,7 +16,7 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
 
     static {
         inMemoryUserList.add(new JwtUserDetails(1L, "amandaw",
-                "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
+                "$2y$12$UcBbXfbhT8OAwEGhnYXP2e0GWXF8ShiYHUB89rOrdaPV5uFvi8t2u", "ROLE_USER_2"));
     }
 
     @Override
